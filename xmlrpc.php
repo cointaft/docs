a5f3dd52c11951cfac826a684349fd8c984449d1
cointaft <taftcoin@gmail.com>
2023-11-07 21:55:37 +0700

    cointaft documentation

---
+++ xmlrpc.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>CoinTaft</engineName>
+		<engineLink>https://cointaft.cloud/</engineLink>
+		<homePageLink>https://docs.cointaft.cloud/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://docs.cointaft.cloud/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://docs.cointaft.cloud/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://docs.cointaft.cloud/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://docs.cointaft.cloud/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://docs.cointaft.cloud/json/" />
+			</apis>
+	</service>
+</rsd>
+	
